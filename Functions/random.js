a26f277e71c4bc9e28a7abdd32743a43db9bf660
Paul <paul@clay.graphics>
2018-09-27 19:11:07 +0100

    Functions - tested to check values are numbers

--- Functions/random.js
+++ Functions/random.js
@@ -1,16 +1,29 @@
 // Functions are usually put at the top 
 // Function parameters sit inside the parenthesis
-function getRandomNumber( upper ) {
-    var randomNumber = Math.floor( Math.random() * upper) + 1;
-    return randomNumber;
-}
-
+// function getRandomNumber( upper ) {
+//     var randomNumber = Math.floor( Math.random() * upper) + 1;
+//     return randomNumber;
+// }
 // console.log ( getRandomNumber(6) );
 // console.log ( getRandomNumber(100) );
 // console.log ( getRandomNumber(10000) );
 
-function getArea (width, length, unit) {
-    var area = width * length;
-    return area + " " + unit;
+// Adding more than one parameter
+// function getArea (width, length, unit) {
+//     var area = width * length;
+//     return area + " " + unit;
+// }
+// console.log (getArea(10,20, 'sq ft'));
+
+
+function getRandomNumber (lower, upper) {
+    if ( isNaN(lower) || isNaN(upper) ) {
+        throw new Error ('error message');
+    }
+    return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
-console.log (getArea(10,20, 'sq ft'));
+
+console.log(getRandomNumber (0, 10));
+console.log(getRandomNumber (100, 1000));
+console.log(getRandomNumber ('nine', 10));
+document.write(getRandomNumber (0, 10000));
