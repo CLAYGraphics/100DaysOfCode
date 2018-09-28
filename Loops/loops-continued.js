ef4e6bfa7f9a275da8de9a103086fff87d2c4706
Paul <paul@clay.graphics>
2018-09-28 23:01:11 +0100

    For Loops work

--- Loops/loops-continued.js
+++ Loops/loops-continued.js
@@ -1,16 +1,36 @@
-var upper = 10000;
-var randomNumber = getRandomNumber(10000);
+// var upper = 10000;
+// var randomNumber = getRandomNumber(10000);
+// var guess;
+// var attempts = 0;
+
+// function getRandomNumber (upper) {
+//     return Math.floor( Math.random() * upper) + 1;
+// }
+
+// while (guess !== randomNumber) {
+//     guess = getRandomNumber( upper );
+//     attempts +=1;
+// }
+
+// document.write('<h1>The random number was: ' + randomNumber + '</h1>');
+// document.write('<h2>The computer took ' + attempts + ' attempts to get it right!</h2>');
+
+var randomNumber = getRandomNumber(10);
 var guess;
-var attempts = 0;
+var guessCount = 0;
+var correctGuess = false;
 
 function getRandomNumber (upper) {
-    return Math.floor( Math.random() * upper) + 1;
+    var num = Math.floor( Math.random() * upper) + 1;
+    return num;
 }
 
-while (guess !== randomNumber) {
-    guess = getRandomNumber( upper );
-    attempts +=1;
-}
-
-document.write('<h1>The random number was: ' + randomNumber + '</h1>');
-document.write('<h2>The computer took ' + attempts + ' attempts to get it right!</h2>');
+do {
+    guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+    guessCount += 1;
+    if (parseInt(guess) === randomNumber) {
+        correctGuess = true;
+    }
+} while( ! correctGuess )
+document.write('<h1>You guessed the number!</h1>');
+document.write('It took you ' + guessCount + ' guesses ');
