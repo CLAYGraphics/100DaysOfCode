5ec69f6e80240314493fbd48ce786bb50913d87f
Paul <paul@clay.graphics>
2018-10-01 22:05:07 +0100

    Continued working on Arrays

---
+++ Arrays/Music Playlist/js/playlist.js
@@ -0,0 +1,66 @@
+// var playlist = [];
+
+// playlist.push("Can I Kick It?");
+// playlist.push("Baby I'm Yours", "Wonderwall");
+// playlist.unshift("At Last");
+// playlist.unshift("Runnin'", "All The Places");
+// printList( playlist );
+
+// var students = [
+//     "Paul", 
+//     "Dave", 
+//     "Chris", 
+//     "Mark"
+// ];
+
+// for (var i = 0; i < students.length; i += 1) {
+//     console.log(students[i]);
+// }
+
+var playlist = [
+    'Can I Kick It?',
+    'Runnin',
+    'All The Places',
+    'Heavy, California',
+    'Unfinished Sympathy'
+];
+
+function print (message) {
+    document.write(message);
+}
+
+function printList( list ) {
+    var listHTML = '<ol>';
+    for (var i = 0; i < list.length; i += 1) {
+        listHTML += '<li>' + list[i] + '</li>'
+    }
+    listHTML += '</ol>';
+    print(listHTML);
+} 
+printList(playlist);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------
+
+// var daysInThewWeek = [
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//     "Sunday"   
+// ];
+// var daysString = daysInThewWeek.join(', ');
+// document.write(daysString);
---
+++ Arrays/shopping.js
@@ -0,0 +1,10 @@
+var shoppingList = [
+    "Carrots",
+    "Apples",
+    "Milk"
+];
+
+// alert(shoppingList[1]);
+// shoppingList.length
+
+shoppingList.push("Eggs");
