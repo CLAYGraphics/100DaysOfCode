e795cdfabfc3bf377e53e042be4abb8d2f041dc4
Paul <paul@clay.graphics>
2018-10-04 18:07:39 +0100

    Starting Objects

---
+++ Objects/js/object.js
@@ -0,0 +1,18 @@
+// var student = { 
+//     name: "Dave",
+//     age: 20,
+//     grades: [80, 95, 87, 85]
+// };
+
+var person = {
+    name: 'Bob',
+    country: 'UK',
+    age: 42,
+    collegeStudent: true,
+    skills: ['HTML', 'CSS', 'Design' ]
+};
+ alert( person.name );
+ person.name = 'Bobby';
+ person.gender = 'Male';
+ alert( person.name );
+ alert( person.gender );
