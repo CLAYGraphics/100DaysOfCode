1fe3226f944e4fc7a1f7d06500d2b35fd5a404c6
Paul <paul@clay.graphics>
2018-10-04 18:33:22 +0100

    For In Loop work

--- Objects/js/object.js
+++ Objects/js/object.js
@@ -1,18 +1,36 @@
-// var student = { 
-//     name: "Dave",
-//     age: 20,
-//     grades: [80, 95, 87, 85]
-// };
-
 var person = {
     name: 'Bob',
     country: 'UK',
     age: 42,
     collegeStudent: true,
-    skills: ['HTML', 'CSS', 'Design' ]
+    skills: ['HTML', 'CSS', 'Design', 'Photography', 'JavaScript'],
+    gender: 'Male'
 };
- alert( person.name );
- person.name = 'Bobby';
- person.gender = 'Male';
- alert( person.name );
- alert( person.gender );
+//  alert( person.name );
+//  person.name = 'Bobby';
+//  person.gender = 'Male';
+//  alert( person.name );
+//  alert( person.gender );
+
+
+function print(message) {
+    var div = document.getElementById('listDiv');
+    div.innerHTML = message;
+}
+
+var message = '<p>Hello. My name is ' + person.name + '</p>';
+message += '<p>I live in the ' + person.country + '</p>';
+person.name = 'Bobby';
+message += '<p>But people call me ' + person.name + '</p>';
+person.age += 1;
+message += '<p>My age is ' + person.age + '</p>'; 
+message += '<p>I have ' + person.skills.length + ' skills</p>';
+message += '<p>I think my best skill is ' + person.skills[2] + ' but I really want to improve my ' + person.skills[1] + ' skills</p>';
+message += '<p>A list of skills I have are ' + person.skills.join(', ') + '</p>';
+print(message);
+
+// This is a For In Loop. You can't use dot notation e.g ".name" / Instead use square brackets [ ]
+// In example below the prop variable can be named anything such as key, personData 
+for ( prop in person ) {
+    console.log(prop, ': ', person[prop]) ;
+}
